462bb43920681126f168d389a895283438e2aed9
PARK HYUN EUNG <yaya05097@naver.com>
2023-08-03 16:23:27 +0900

    테이블은 테스트용으로 쓰느라 만들었습니다. 기능 추가하면서 테이블 추가할지 여부 파악하도록 하겠습니다

--- main/java/payment/PayDAO.java
+++ main/java/payment/PayDAO.java
@@ -1,9 +1,48 @@
 package payment;
 
+import java.sql.*;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 public class PayDAO {
+	
+	private PreparedStatement pstmt;
+	private Connection conn;
+	private DataSource dataFactory;
+	
+	public PayDAO() {
+		try {
+			Context ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			dataFactory = (DataSource) envContext.lookup("jdbc/mysqlpool");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	
 	public void addPay(PayVO payvo) {
+		try {
+			conn = dataFactory.getConnection();
+			String merchant_uid = payvo.getMerchant_uid();
+			String amount = payvo.getAmout();
+			String pay_method = payvo.getPay_method();			
+			String query = "INSERT INTO t_payment(merchant_uid, amount, pay_method)" + " VALUES(?, ? ,?)";
+			System.out.println(query);
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, merchant_uid);
+			pstmt.setString(2, amount);
+			pstmt.setString(3, pay_method);			
+			pstmt.executeUpdate();
+			pstmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 }
