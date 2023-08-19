505abe9b2d6e93a69fce0b1eecd6891058814b93
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2023-08-19 19:15:43 +0900

    member 회원가입 정리, 테스트코드 추가

--- main/java/member/SignUpController.java
+++ main/java/member/SignUpController.java
@@ -3,15 +3,25 @@ package member;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/membersignup")
+@WebServlet("/member/join")
 public class SignUpController extends HttpServlet {
 	MemberDAO dao;
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		RequestDispatcher dispatch = request.getRequestDispatcher("/member/mSignup.jsp");
+		dispatch.forward(request, response);
+	}
+
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
